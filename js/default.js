54790846b6a4e51114240eb25234a00000edc8f6
紅月九音 <teiruzuof.15@gmail.com>
2017-06-26 17:55:56 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 for(var i = 1; i < 100; i++){
   var li = document.createElement('li');
   if((i % 3 == 0) && (i % 5 == 0))
-  return li.textContent = 'FizzBuzz';
+    li.textContent = 'FizzBuzz';
   else if(i % 3 == 0){
    return li.textContent = 'Fizz';
   }else if(i % 5 == 0){
