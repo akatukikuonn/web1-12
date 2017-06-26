7d111c994801e9ed9fc71dd84135aeb48f237ada
紅月九音 <teiruzuof.15@gmail.com>
2017-06-26 17:58:52 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,12 +2,12 @@ for(var i = 1; i < 100; i++){
   var li = document.createElement('li');
   if(i % 3 == 0 && i % 5 == 0)
     li.textContent = 'FizzBuzz';
-  else if(i % 3 == 0){
+  else if(i % 3 == 0)
     li.textContent = 'Fizz';
-  }else if(i % 5 == 0){
+  else if(i % 5 == 0)
     li.textContent = 'Buzz';
-  } else {
+  else
     li.textContent = i + '回数';
   document.getElementById('list').appendChild(li);
-}
+  }
   
