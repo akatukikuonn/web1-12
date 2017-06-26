cef5ca29c9a79c4b19744156d59858495a62fc0c
紅月九音 <teiruzuof.15@gmail.com>
2017-06-26 17:43:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,12 +1,12 @@
 for(var i = 1; i < 100; i++){
   var li = document.createElement('li');
-  if((i % 3 == 0) && (i % 5 == 0));
-     li. textContent = 'FizzBuzz';
-  else if(i % 3 == 0);
+  if((i % 3 == 0) && (i % 5 == 0))
+  { li.textContent = 'FizzBuzz';
+  }else if(i % 3 == 0){
     li.textContent = 'Fizz';
-  else if(i % 5 == 0);
+  }else if(i % 5 == 0){
     li.textContent = 'Buzz';
-  else 
+  } else {
     li.textContent = i;
   document.getElementById('list').appendChild(li);
 }
